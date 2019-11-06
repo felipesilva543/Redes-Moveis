ffa72d4664c4a323c17682d73d24e35f596ece31
felipesilva543 <felipesilva543@alu.ufc.br>
2019-11-05 23:26:54 -0300

    testes iniciais do sensor de umidade terrestre e ba bomba de água

--- Proj-Final/Proj-Final.ino
+++ Proj-Final/Proj-Final.ino
@@ -1,51 +1,29 @@
-/*
-  Attivazione_pompa.ino
-  Il programma attiva un rele' a cui puÃ² essere collegata una pompa
-  oppure un'elettrovalvola per irrigare per esempio dei vasi quando siete in vacanza.
-  L'attivazione viene fatta in base alla lettura dell'umiditÃ  del terreno eseguita
-  da un sensore dotato di due terminali, in base alla conducibilitÃ  del terreno,
-  in valore analogico in uscita varia.  Impostando la soglia il relÃ¨ viene eccitato.
-
-  Sono utilizzati i seguenti pin
-  Pin +5V           -> Alimentazione rele'
-  Pin GND           -> GND sensore e relÃ¨
-  Pin Analogico 0   -> Uscita sensore
-  Pin Digitale 2    -> Rele'
-  Pin Digitale 3    -> Sensore
-
-  Creato il 22/1/2017 da Adriano Gandolfo
-  Sito web http://www.adrirobot.it
-  Blog http://it.emcelettronica.com/author/adrirobot
-  Pagina Facebook https://www.facebook.com/Adrirobot-318949048122955
-  Istagram https://www.instagram.com/adrirobot/
-  This example code is in the public domain.
-*/
-
-int lettura = 0;
-int pompa = 2; //Pin a cui Ã¨ collegato i rele' per attivare al pompa
-int sensore = 3; //Pin di alimentazione del sensore
+int LEITURA = 0;   //PINO DE LEITURA DO SENSOR
+#define BOMBA       2   //PINO QUE ATIVA A BOMBA
+#define SENSOR      3  //PINO DE ALIMENTAÇÃO DO SENSOR
+#define PIN_SENSOR  A0
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode (pompa, OUTPUT); // Imposta il pin 2 POMPA come uscita.
-  pinMode (sensore, OUTPUT); //Imposta il pin 2 SENSORE come uscita.
+  pinMode (BOMBA, OUTPUT);  
+  pinMode (SENSOR, OUTPUT); 
 }
 
 void loop()
 {
-  digitalWrite(sensore, HIGH); // Accende sensore
-  lettura = analogRead(A0);
+  digitalWrite(SENSOR, HIGH); // Accende SENSOR
+  LEITURA = analogRead(PIN_SENSOR);
   Serial.print("Valore umidita':");
-  Serial.println(lettura);
-  if (lettura < 200 ) //inizia la lettura: se il valore del sensore Ã¨ inferiore a "200", allora...
+  Serial.println(LEITURA);
+  if (LEITURA > 200 ) //inizia la LEITURA: se il valore del SENSOR Ã¨ inferiore a "200", allora...
   {
-    digitalWrite(pompa, LOW); //... attiva la pompa - lOGICA INVERTITA
+    digitalWrite(BOMBA, LOW); //... attiva la BOMBA - lOGICA INVERTITA
   }
   else //..altrimenti...
   {
-    digitalWrite(pompa, HIGH); //...disattiva la pompa  - lOGICA INVERTITA
+    digitalWrite(BOMBA, HIGH); //...disattiva la BOMBA  - lOGICA INVERTITA
   }
-  digitalWrite(sensore, LOW); // Spegne sensore
+  digitalWrite(SENSOR, LOW); // Spegne SENSOR
   delay(1000);
 }
